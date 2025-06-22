7879b1f3ba44a1acc1cb8e9f409364f632664519
Block Chain Dev <145947093+horaciolidity@users.noreply.github.com>
2025-06-21 23:30:50 -0300

    Update emisor-cliente.js

--- emisor-cliente.js
+++ emisor-cliente.js
@@ -1,12 +1,19 @@
-// emisor-cliente.js
 const canal = new BroadcastChannel('pos_channel');
 let totalCliente = 0;
 
-function notificarCliente(nombre, precio) {
-  totalCliente += precio;
+function notificarCliente(nombre, precio, cantidad) {
+  const subtotal = precio * cantidad;
+  totalCliente += subtotal;
   canal.postMessage({
+    tipo: 'producto',
     nombre,
     precio,
+    cantidad,
+    subtotal,
     totalActual: totalCliente
   });
+}
+function resetCliente() {
+  totalCliente = 0;
+  canal.postMessage({ tipo: 'reset' });
 }
